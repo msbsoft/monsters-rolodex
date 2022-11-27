41f26316e8e429c46ddefd695a92d4398d164f02
manpreet.singh+git@gmail.com <manpreet.singh+git@gmail.com>
2022-11-27 11:12:32 +0800

    First impl. of filtering monsters using separate state to store filtered monsters. Not optimal.

--- src/components/app/App.component.jsx
+++ src/components/app/App.component.jsx
@@ -5,18 +5,32 @@ import CardList from '../card-list/card-list-component';
 
 function App() {
   const [monsters, setMonsters] = useState([])
-  
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+
   useEffect(() => {
     async function getMonsters(){
-      let monsters = await axios.get(`https://jsonplaceholder.typicode.com/users`)
-      setMonsters(monsters.data)    
+      let monsters = await (await axios.get(`https://jsonplaceholder.typicode.com/users`)).data
+      setMonsters(monsters)
+      setFilteredMonsters(monsters);    
     }
     getMonsters()
   }, [])
 
+  const handleSearch = (e) => {    
+    if (!e.target.value) {
+      setFilteredMonsters(monsters)
+    }  
+    
+    let filteredMonsters = monsters.filter((monster) => 
+      monster.name.toLowerCase().includes(e.target.value.toLowerCase())) 
+    
+    setFilteredMonsters(filteredMonsters)
+  }
+
   return (
-    <div className="App">
-      <CardList monsters={monsters} />
+    <div className="App">    
+      <input type="search" onChange={handleSearch} placeholder="Search Monsters"/>
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
